567f2461d41a560564da95c02f20d2ef8ff0e49b
Petr Krysl <pkrysl@ucsd.edu>
2020-07-06 11:21:23 -0700

    explain exports

--- src/MeshSteward.jl
+++ src/MeshSteward.jl
@@ -3,17 +3,22 @@ module MeshSteward
 include("boxes.jl")
 include("vselect.jl")
 include("eselect.jl")
-
 include("utilities.jl")
 include("tetrahedra.jl")
 include("triangles.jl")
 include("quadrilaterals.jl")
-
 include("io.jl")
 include("modification.jl")
-
 include("mesh.jl")
 
+# We can either use/import individual functions from MeshSteward like so:
+# ```
+# using MeshSteward: attach!
+# ```
+# or we can bring into our context all exported symbols as
+# ```
+# using MeshSteward.Exports
+# ```
 include("Exports.jl")
 
 end # module
